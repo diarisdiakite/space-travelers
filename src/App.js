3128014fb0f0c8f92717d0483fc29cec77562172
diarisdiakite <diarisdiakite@gmail.com>
2023-08-28 21:49:48 +0200

    Set up react. Add linters.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome on Space-X
+      </header>
+    </div>
+  );
+}
+
+export default App;
