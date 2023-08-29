c91ca19c1af182ff665c64556090f022a7b48e54
kambereBr <brunokambere@gmail.com>
2023-08-30 01:34:41 +0200

    update the image path

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../images/planet.png';
+import logo from '../assets/images/planet.png';
 import '../assets/css/navbar.css';
 
 const NavBar = () => (
