9f924a3f9e1df89e34f43bbaf363cf8836654a8e
kambereBr <brunokambere@gmail.com>
2023-08-29 17:30:04 +0200

    clean App.js file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import {
-  BrowserRouter, Routes, Route,
+  BrowserRouter,
 } from 'react-router-dom';
-// import './assets/css/missions.css';
-import Mission from './features/missions/Mission';
-import MissionsList from './features/missions/MissionsList';
-import UsersList from './features/users/UsersList';
-import User from './features/users/User';
-// import NotFoundPage from './components/pages/NotFoundPage';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MissionsList />} />
-        {' '}
-        {/* This should be the rocketsLit, we may change it */}
-        <Route path="/missions">
-          <Route index element={<MissionsList />} />
-          <Route path=":missionId" element={<Mission />} />
-        </Route>
-        <Route path="/users">
-          <Route index element={<UsersList />} />
-          <Route path=":userId" element={<User />} />
-        </Route>
-      </Routes>
+      <NavBar />
     </BrowserRouter>
   );
 }
