340205fe1133b88dd0f7983765226ea201510f1e
diarisdiakite <diarisdiakite@gmail.com>
2023-08-29 18:12:41 +0200

    Display image in the navbar. Add classes.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../images/planet.png';
+import '../assets/css/navbar.css';
 
 const NavBar = () => (
-  <div>
-    <div className="nav-title">
-      <image src={logo} alt="logo image" />
+  <div className="container">
+    <div className="title">
+      <img src={logo} className="logo" alt="logo" />
+      <h2>Space travelers</h2>
     </div>
-    <nav>
-      <NavLink to="/Rockets">Rockets</NavLink>
-      <NavLink to="/Rockets">Missions</NavLink>
-      <NavLink to="/Rockets">My Profile</NavLink>
+    <nav className="nav-container">
+      <NavLink to="/Rockets" className="nav-item">Rockets</NavLink>
+      <NavLink to="/Rockets" className="nav-item">Missions</NavLink>
+      <NavLink to="/Rockets" className="nav-item">My Profile</NavLink>
     </nav>
   </div>
 );
