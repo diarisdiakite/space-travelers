de1e9e9493b7f05f9975ed9a3fe2c751d0b9595b
diarisdiakite <diarisdiakite@gmail.com>
2023-08-29 03:18:40 +0200

    Add a user feature and components.

---
+++ src/features/users/UsersList.js
@@ -0,0 +1,38 @@
+import { Link, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectAllUsers } from './usersSlice';
+import { selectMissionsByUser } from '../missions/missionsSlice';
+
+function UsersList() {
+  const { userId } = useParams();
+  const users = useSelector(selectAllUsers);
+
+  const userMissions = useSelector((state) => selectMissionsByUser(state, Number(userId)));
+
+  const content = users.map((user) => (
+    <div key={user.id}>
+      <h2 key={user.id}>
+        <Link to={`/users/${user.id}`}>
+          {user.name}
+          {' '}
+          (
+          {/* calculate user length */}
+          {userMissions.length}
+          )
+        </Link>
+      </h2>
+      <p key={`description-${user.id}`}>
+        {user.description}
+        {' '}
+      </p>
+    </div>
+  ));
+
+  return (
+    <div className="main-container">
+      {content}
+    </div>
+  );
+}
+
+export default UsersList;
