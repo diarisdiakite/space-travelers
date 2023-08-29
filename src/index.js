31c29f4d371c3957b59673f4583b97000c880969
kambereBr <brunokambere@gmail.com>
2023-08-29 17:32:40 +0200

    Clean index.js file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './features/store';
 import './index.css';
 import App from './App';
-import { fetchMissions, setFetchedMissions } from './features/missions/missionsSlice';
-
-store.dispatch(fetchMissions());
-store.dispatch(setFetchedMissions());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
 );
