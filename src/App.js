974428b5fe334d4b9674b814b16e7aa91460638b
kambereBr <brunokambere@gmail.com>
2023-08-30 01:35:31 +0200

    Add route to the Rockets component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import {
-  BrowserRouter,
+  BrowserRouter, Routes, Route,
 } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import RocketsList from './features/rockets/RocketsList';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      <Routes>
+        <Route path="/Rockets" element={<RocketsList />} />
+      </Routes>
     </BrowserRouter>
   );
 }
