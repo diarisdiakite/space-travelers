f29732407d014950bcac6523dd7038f4fd05c9f4
diarisdiakite <diarisdiakite@gmail.com>
2023-08-29 02:49:13 +0200

    Add the provider in index.js. add routes in App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
+// import './assets/css/missions.css';
+import Mission from './features/missions/Mission';
+import MissionsList from './features/missions/MissionsList';
+import UsersList from './features/users/UsersList';
+import User from './features/users/User';
+// import NotFoundPage from './components/pages/NotFoundPage';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Welcome on Space-X
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MissionsList />} />
+        {' '}
+        {/* This should be the rocketsLit, we may change it */}
+        <Route path="/missions">
+          <Route index element={<MissionsList />} />
+          <Route path=":missionId" element={<Mission />} />
+        </Route>
+        <Route path="/users">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<User />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
