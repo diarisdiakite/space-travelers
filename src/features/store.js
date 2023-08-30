98195c0ef211643828f7a2d94ab0c9271a8ac7a1
kambereBr <brunokambere@gmail.com>
2023-08-30 22:36:26 +0200

    import rocketsSlice into store.js

--- src/features/store.js
+++ src/features/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missionsSlice';
 import usersReducer from './users/usersSlice';
+import rocketsSlice from './rockets/rocketsSlice';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
     users: usersReducer,
+    rockets: rocketsSlice,
   },
 });
 
