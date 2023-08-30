e9c66a301c2b67be0fe476287fd643b64e492737
kambereBr <brunokambere@gmail.com>
2023-08-30 18:59:10 +0200

    Revert "Clean store.js file"
    This reverts commit b505d957739d044df320ca5eedcf3a12b1567594.

--- src/features/store.js
+++ src/features/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import missionsReducer from './missions/missionsSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    missions: missionsReducer,
+  },
 });
 
 export default store;
