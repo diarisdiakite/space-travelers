5219142a84aa1aa17e031155915809ca8c3db446
kambereBr <brunokambere@gmail.com>
2023-08-30 19:00:42 +0200

    Revert "clean App.js file"
    This reverts commit 9f924a3f9e1df89e34f43bbaf363cf8836654a8e.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import {
-  BrowserRouter,
+  BrowserRouter, Routes, Route,
 } from 'react-router-dom';
-import NavBar from './components/NavBar';
+// import './assets/css/missions.css';
+import Mission from './features/missions/Mission';
+import MissionsList from './features/missions/MissionsList';
+import UsersList from './features/users/UsersList';
+import User from './features/users/User';
+// import NotFoundPage from './components/pages/NotFoundPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
+      <Routes>
+        <Route path="/" element={<MissionsList />} />
+        {' '}
+        {/* This should be the rocketsLit, we may change it */}
+        <Route path="/missions">
+          <Route index element={<MissionsList />} />
+          <Route path=":missionId" element={<Mission />} />
+        </Route>
+        <Route path="/users">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<User />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
