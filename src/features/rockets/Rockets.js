2f8d9cfa6dc362de5aa22855ad8a2e8707045319
kambereBr <brunokambere@gmail.com>
2023-08-30 18:45:28 +0200

    handle reserve click

--- src/features/rockets/Rockets.js
+++ src/features/rockets/Rockets.js
@@ -1,28 +1,43 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { reserveAction } from '../../redux/rockets/rocketsSlice';
 import '../../assets/css/rockets.css';
 
 const Rockets = ({
-  id, rocketName, description, flickrImages,
-}) => (
-  <div className="main-container">
-    <div className="rocket-card">
-      <div className="rocket-image-container">
-        <img src={flickrImages} alt="rocket" />
-      </div>
-      <div className="rocket-content-container">
-        <h2 key={id}>{rocketName}</h2>
-        <p>{description}</p>
-        <button type="button" className="reserve-button">Reserve Rocket</button>
+  id, rocketName, description, flickrImages, reserved = false,
+}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="main-container">
+      <div className="rocket-card">
+        <div className="rocket-image-container">
+          <img src={flickrImages} alt="rocket" />
+        </div>
+        <div className="rocket-content-container">
+          <h2 key={id}>{rocketName}</h2>
+          <p>
+            <span className={reserved ? 'reserved-badge' : ''}>{reserved ? 'Reserved' : ''}</span>
+            {description}
+          </p>
+          {reserved && (
+            <button type="button" className="cancel-button" onClick={() => dispatch(reserveAction(id))}>Cancel Reservation</button>
+          )}
+          {!reserved && (
+            <button type="button" className="reserve-button" onClick={() => dispatch(reserveAction(id))}>Reserve Rocket</button>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 Rockets.propTypes = {
   id: PropTypes.string.isRequired,
   rocketName: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   flickrImages: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 export default Rockets;
