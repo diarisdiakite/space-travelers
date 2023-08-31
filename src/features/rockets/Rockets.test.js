99d32d27aa97c3aa5264c2a3f51453be28afdd88
kambereBr <brunokambere@gmail.com>
2023-08-31 21:56:34 +0200

    Enhance Rockets test to handle data properly

--- src/features/rockets/Rockets.test.js
+++ src/features/rockets/Rockets.test.js
@@ -8,16 +8,26 @@ import imageError from '../../assets/images/image_not_available.png';
 
 describe('Rockets component', () => {
   it('renders correctly', () => {
+    const data = [
+      {
+        id: '5e9d0d95eda69955f709d1eb',
+        name: 'Falcon 1',
+        type: 'rocket',
+        flickr_images: imageError,
+        description: 'The Falcon 1 was an expendable launch system privately',
+        reserved: false,
+      },
+    ];
     const tree = renderer
       .create(
         <Provider store={store}>
           <MemoryRouter>
             <Rockets
-              id="1"
-              rocketName="a"
-              description="a"
-              flickrImages={imageError}
-              reserved={false}
+              id={data[0].id}
+              rocketName={data[0].name}
+              description={data[0].description}
+              flickrImages={data[0].flickr_images}
+              reserved={data[0].reserved}
             />
           </MemoryRouter>
         </Provider>,
